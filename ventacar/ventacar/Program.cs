d53310de9d1a29beadbcc6aad0da349fcefe90e2
Néstor Martínez Hernandez <nestormartinezh@outlook.com>
2017-07-16 14:03:02 -0500

    integracion con base de datos

--- ventacar/ventacar/Program.cs
+++ ventacar/ventacar/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace ventacar
 {
@@ -16,6 +17,24 @@ namespace ventacar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source = Database.mdb");
+            try
+
+            {
+
+                conexion.Open();
+
+                conexion.Close();
+
+            }
+
+            catch (System.Exception ex)
+
+            {
+
+                MessageBox.Show("ERROR:" + ex.Message);
+
+            }
             Application.Run(new Form1());
         }
     }
